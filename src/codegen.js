4752745fab146b519c4f99186e924f5e157c0be2
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-06 17:37:50 -0800

    started codegenerator

--- src/codegen.js
+++ src/codegen.js
@@ -1,7 +1,25 @@
 //file for node-wise transcompiling functions and generating code
 
-var codeGenFuncs = {
-	"+":function(){}
-};
+var CodeGenerator = (function(){
+	function CodeGenerator(lib){
+		//starts with either a core object library or nothing
+		lib = lib || {};
+		for(var name in lib){
+			this[name] = lib[name];
+		}
+	}
+	CodeGenerator.prototype.set = function(name, func) {
+		this[name] = func;
+	};
+	//safe version of set
+	CodeGenerator.prototype.add = function(name, func) {
+		if(!(name in this)) this[name] = func;
+	};
 
-exports.codeGenFuncs = codeGenFuncs;
+	CodeGenerator.prototype.get = function(name) {
+		return this[name];
+	};
+	return CodeGenerator;
+})();
+
+exports.CodeGenerator = CodeGenerator;
