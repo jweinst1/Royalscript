bad1b22fd6d5a3d8ae48b703d140b8457cd5bcbe
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-07 12:44:54 -0800

    stdlib progress

--- src/stdlib.js
+++ src/stdlib.js
@@ -1,7 +1,22 @@
 //standard library object
 
 var STD = {
-
+	"+":function(args, spacer){
+		var string = spacer;
+		if (typeof args[0] === 'string' && typeof args[1] === 'object') { string += this[args[0]](args[1], spacer);} else{string += args[0]};
+		for (var i = 1; i < args.length; i++) {
+			if (typeof args[i] === 'string' && typeof args[i+1] === 'object') { string += " + " + this[args[i]](args[i+1], spacer);} else{string += " + " + args[i]};
+		};
+		return string;
+	}
 };
 
 exports.STD = STD;
+
+//utility function used to recursively call nested arguments from the lib.
+function callFromLib(lib, name, args, spacer){
+	if(!(name in lib)) throw "Name Error: Function " + name + " not found.";
+	else return lib[name](args, spacer);
+}
+
+console.log(STD["+"]([ '+', [ 1, 2, '+', [ 5, 6 ] ] ], ""));
