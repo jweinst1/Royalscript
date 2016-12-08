fbbaf7a40f88ee41b091240bff474b215a8ef6a6
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-07 17:34:18 -0800

    string change to ``

--- src/RoyalParse.js
+++ src/RoyalParse.js
@@ -6,16 +6,32 @@ function checkForBad(string){
 	return string.search(/\{\}|\[\]/) !== -1;
 }
 
+//prepares `` strings for parser
+function padIfString(string){
+	if(/^`[^`]*`$/.test(string)) return '"`' + string.slice(1, -1) + '`"';
+	else return string;
+}
+
+//cleans undefined and '' from token list
+function clean(arr){
+	var newarr = [];
+	for (var i = 0; i < arr.length; i++) {
+		if(arr[i]) newarr.push(arr[i]);
+	};
+	return newarr;
+}
+
 var RoyalParse = function(code){
 
 		  if(checkForBad(code)) throw "Illegal Parenthesis Error";
-		  var tokens = code.split(/(\()|(\))|(,)|( )|\n|\t/);
+		  var tokens = clean(code.split(/(^`[^`]*`$)|(\()|(\))|(,)|( )|\n|\t/));
 		  var repdict = {
 		  	"(":"[",
 		  	")":"]",
 		  	"undefined":''
 		  };
 		  for (var i=0;i<tokens.length;i++){
+		  	tokens[i] = padIfString(tokens[i]);
 		  	if(tokens[i] in repdict) tokens[i] = repdict[tokens[i]];
 		  	else if(tokens[i+1] === "("){
 		  		tokens[i] = '"' + tokens[i] + '",';
