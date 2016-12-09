a683bf77310449390bfa6c608ea25886a657d1c2
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-09 13:07:36 -0800

    condition functions and limit infix

--- src/stdlib.js
+++ src/stdlib.js
@@ -10,6 +10,23 @@ var callLib = function(lib, first, second, spacer){
 	}
 };
 
+exports.callLib = callLib;
+
+//util function that unnests only 2 arguments from an AST node, otherwise throws error
+/*var get2Args = function(lib, args, spacer){
+	switch(args.length){
+		case 2:
+		   if(typeof args[1] === 'object') throw "Argument Error: Got improper arguments but expected 2.";
+		   else return [args[0], args[1]];
+		case 3:
+		   break;
+		case 4:
+		   break;
+		default:
+		   throw "Argument Error: Got improper arguments but expected 2.";
+	}
+};*/
+
 //standard library object
 //functions starting with , are private and cannot be called in the front-end
 var STD = {
@@ -25,6 +42,17 @@ var STD = {
 	},
 	//private infix joiner function
 	",infix":function(sep, args, spacer){
+		var str = spacer + callLib(this, args[0], args[1], spacer);
+		for (var i = 1; i < args.length; i++) {
+			if(!(typeof args[i] === 'object')){
+				str += sep + callLib(this, args[i], args[i+1], spacer);
+			}
+		};
+		return str;		
+	},
+	//same as ,infix but has a limit of the amount of arguments it links
+	",ifixn":function(sep, limit, args, spacer){
+		if(args.length < 1 || args.length > limit) throw "Argument Error: Improper number of arguments";
 		var str = spacer + callLib(this, args[0], args[1], spacer);
 		for (var i = 1; i < args.length; i++) {
 			if(!(typeof args[i] === 'object')){
@@ -106,11 +134,26 @@ var STD = {
 		return str;
 	},
 	"==":function(args, spacer){
-		//not implemented
+		return this[",ifixn"](" === ", 4, args, spacer);
+	},
+	"!=":function(args, spacer){
+		return this[",ifixn"](" !== ", 4, args, spacer);
+	},
+	">":function(args, spacer){
+		return this[",ifixn"](" > ", 4, args, spacer);
+	},
+	"<":function(args, spacer){
+		return this[",ifixn"](" < ", 4, args, spacer);
+	},
+	"<=":function(args, spacer){
+		return this[",ifixn"](" <= ", 4, args, spacer);
+	},
+	">=":function(args, spacer){
+		return this[",ifixn"](" >= ", 4, args, spacer);
 	}
 };
 
 exports.STD = STD;
 
-var obj = [ '$', [ '5', '6', '||', [ '3', '4' ], '&&', ['true', 'false'] ] ];
+var obj = [ '$', [ '5', '6', '==', [ '+', ['5', '6'], '5'], '>=', ['true', 'false'] ] ];
 console.log(STD[obj[0]](obj[1], ""));
