fabdb590d1008581351e512c0ef851fce3c312cf
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-09 20:08:41 -0800

    map init defined

--- src/stdlib.js
+++ src/stdlib.js
@@ -186,10 +186,37 @@ var STD = {
 	//collection initializers
 	"list":function(args){
 		return "[" + this[","](args) + "]";
+	},
+	"map":function(args){
+		if(args.length === 0) return "{}";
+		var str = "{";
+		var kmode = true;
+		for (var i = 0; i < args.length; i++) {
+			if(kmode){
+				if(typeof args[i+1] === 'object') throw "Key Error: key must be static string";
+				else {
+					str += args[i] + ":";
+					kmode = false;
+				}
+			}
+			else {
+				if(typeof args[i+1] === 'object'){
+					str += callLib(this, args[i], args[i+1]) + ", ";
+					i += 1;
+					kmode = true;
+				}
+				else {
+					str += args[i] + ", ";
+					kmode = true;
+				}
+			}
+		};
+		if(!(kmode)) str += "null";
+		return str + "}";
 	}
 };
 
 exports.STD = STD;
 
-var obj = [ '=', ['r', 'list', []]];
+var obj = [ '=', ['r', 'map', []]];
 console.log(STD[obj[0]](obj[1]));
