e79e38191ab31901285da14475c9f86fc5874305
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-10 20:20:24 -0800

    implemented proper executor classes

--- src/exec.js
+++ src/exec.js
@@ -1,13 +1,44 @@
 //JS code executor object
 
-function Executor(){
-	this.code = "";
-	this.result = undefined;
-	this.addCode = function(string){
+//node exeuctor version
+var Executor = (function(){
+	function Executor(){
+		this.code = "";
+		this.result = undefined;
+	}
+
+	Executor.prototype.addCode = function(string){
 		this.code += string;
 	};
-	this.exec = function(){
+	Executor.prototype.exec = function(){
 		this.result = eval(this.code);
 		return this.result;
 	};
-}
+	return Executor;
+})();
+
+exports.Executor = Executor;
+
+
+var WebExecutor = (function(){
+	function WebExecutor(){
+		this.code = "";
+		this.result = undefined;
+	}
+
+	WebExecutor.prototype.addCode = function(string){
+		this.code += string;
+	};
+	WebExecutor.prototype.exec = function(){
+		var oldLog = console.log;
+		console.log = function(message, textout){
+			//implement textout here for custom output
+			oldLog.apply(console, arguments);
+		};
+		this.result = eval(this.code);
+		return this.result;
+	};
+	return WebExecutor;
+})();
+
+exports.WebExecutor = WebExecutor;
