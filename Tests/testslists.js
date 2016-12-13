42b13ab4dee5f9e2cc58a3fe6c863e7033f15932
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-13 15:27:20 -0800

    more list tests

--- Tests/testslists.js
+++ Tests/testslists.js
@@ -4,3 +4,10 @@ var t = require("./core.js");
 /*Begin Tests*/
 
 t.compileTest("list()", "[]", 1);
+t.compileTest("list(1)", "[1]", 2);
+t.compileTest("list(1, 2)", "[1, 2]", 3);
+t.compileTest("list(1, 2, 3, 4, list(1))", "[1, 2, 3, 4, [1]]", 4);
+t.compileTest("list(+(1, 5))", "[1 + 5]", 5);
+t.compileTest("list(**(1, 5))", "[Math.pow(1, 5)]", 6);
+t.compileTest("list(@(arg, *(arg, arg)))", "[function(arg){return arg * arg}]", 7);
+t.compileTest("list(true, false)", "[true, false]", 8);
