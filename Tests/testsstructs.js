40167f6940fd050c8172dd865726c1257b00cd03
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-13 12:34:37 -0800

    struct unit tests completed

--- Tests/testsstructs.js
+++ Tests/testsstructs.js
@@ -9,3 +9,5 @@ t.compileTest("struct(fun, soup)", "function fun(soup){this.soup = soup;};", 2);
 t.compileTest("struct(alphabet)", "function alphabet(){};", 3);
 t.compileTest("new(a, 3, 4)", "new a(3,4)", 4);
 t.compileTest("new(b)", "new b()", 5);
+t.compileTest("new(a, +(3, 4), 4)", "new a(3 + 4,4)", 6);
+t.compileTest("new(a, list(3, 4, 5), 4)", "new a([3, 4, 5],4)", 7);
