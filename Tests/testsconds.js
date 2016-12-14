fc4042b945cbc8eeb7824797e13bdaa0fc2c6e09
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-13 19:38:09 -0800

    more cold tests

--- Tests/testsconds.js
+++ Tests/testsconds.js
@@ -10,3 +10,7 @@ t.compileTest("&&(3, 4, 5)", "3 && 4 && 5", 4);
 t.compileTest("same(4, 4)", "JSON.stringify(4) === JSON.stringify(4)", 5);
 t.compileTest("same(list(), list(1))", "JSON.stringify([]) === JSON.stringify([1])", 6);
 t.compileTest("?(not(false), $(8))", "if(!(false)){console.log(8);};", 7);
+t.compileTest("?(==(3, 7), $(8))", "if(3 === 7){console.log(8);};", 8);
+t.compileTest("if(==(3, 7), $(8), $(4))", "if(3 === 7){console.log(8);} else{console.log(4);};", 9);
+t.compileTest("ifs(true, return(1, 2), ==(3, 3), $(4))", "if(true){return [1, 2];};if(3 === 3){console.log(4);};", 10);
+t.compileTest("switch(b, 4, $(3), 5, $(4), true, $(1, 2))", "switch(b){case 4: console.log(3);break;case 5: console.log(4);break;case true: console.log(1, 2);break;};", 11);
--- Tests/testsfunctions.js
+++ Tests/testsfunctions.js
@@ -6,7 +6,7 @@ var t = require("./core.js");
 t.compileTest("@(a, +(a, 4))", "function(a){return a + 4}", 1);
 t.compileTest("@(a, -(a, 4))", "function(a){return a - 4}", 2);
 t.compileTest("@(a, do(+(a, 4)))", "function(a){return a + 4}", 3);
-t.compileTest("do(+(1, 3), =(e, 5))", "1 + 3 var e = 5;", 4);
+t.compileTest("do(+(1, 3), =(e, 5))", "1 + 3\nvar e = 5;", 4);
 t.compileTest("@@(a, b, //(a, b))", "function(a,b){return Math.floor(a / b)}", 5);
 t.compileTest("@@(a, b, ==(a, b))", "function(a,b){return a === b}", 6);
 t.compileTest("@@(a, b, &&(==(a, b), !=(b, 3)))", "function(a,b){return a === b && b !== 3}", 7);
