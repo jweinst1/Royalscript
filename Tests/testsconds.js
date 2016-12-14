504da3fb7ff1737d807dbaef7390363e16443c86
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-13 19:49:27 -0800

    finished conditional tests

--- Tests/testsconds.js
+++ Tests/testsconds.js
@@ -14,3 +14,5 @@ t.compileTest("?(==(3, 7), $(8))", "if(3 === 7){console.log(8);};", 8);
 t.compileTest("if(==(3, 7), $(8), $(4))", "if(3 === 7){console.log(8);} else{console.log(4);};", 9);
 t.compileTest("ifs(true, return(1, 2), ==(3, 3), $(4))", "if(true){return [1, 2];};if(3 === 3){console.log(4);};", 10);
 t.compileTest("switch(b, 4, $(3), 5, $(4), true, $(1, 2))", "switch(b){case 4: console.log(3);break;case 5: console.log(4);break;case true: console.log(1, 2);break;};", 11);
+t.compileTest("ife(==(3, 3), return(3), !=(3, true), $(1, 2, 3, 4), return(list(1, 2, 3)))", "if(3 === 3){return 3;};else if(3 !== true){console.log(1, 2, 3, 4);};else{return [1, 2, 3];};", 12);
+t.compileTest("?(true, ?(3, $(1, 2, 3)))", "if(true){if(3){console.log(1, 2, 3);};};", 13);
