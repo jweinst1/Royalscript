5ccc7003e67f1079112a873277f09f839a18e440
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-14 09:22:25 -0800

    changing main file

--- Main.js
+++ Main.js
@@ -3,12 +3,15 @@
 var cmp = require("./src/compiler.js");
 var fs = require('fs');
 var userArgs = process.argv.slice(2);
+if(userArgs.length < 2) throw "Error mode flag not specified";
 
 
 
 //reads string from file
-//fs.readFile(userArgs[0], 'utf-8', function (err, data) {
+fs.readFile(userArgs[1], 'utf-8', function (err, data) {
     if (err) throw err;
     var transcompiled = cmp.Compile(data);
-    console.log(eval(transcompiled));
+    if(userArgs[0] === '-j') console.log(transcompiled);
+    else if(userArgs[0] === '-e')console.log(eval(transcompiled));
+    else throw "Error mode flag not recognized";
 });
