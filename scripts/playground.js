0356061abeda43d62384f3714a255d0a8a018e44
Josh Weinstein <jweinst1@berkeley.edu>
2016-12-15 12:05:43 -0800

    Update playground.js

--- scripts/playground.js
+++ scripts/playground.js
@@ -1,3 +1,10 @@
 window.addEventListener("DOMContentLoaded", function () {
-    
+       window.editor = CodeMirror.fromTextArea("code", {
+             mode: "scheme",
+             theme: "night"
+        });
+        
+        window.editor.evalRoyalScript = function(input){
+            return input + "proc";
+        };
 }, false);
