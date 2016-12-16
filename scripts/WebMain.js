c5eccfd6ab1cb410461a7b8d8ad4116a7fe4b792
Josh Weinstein <jweinst1@berkeley.edu>
2016-12-16 15:18:28 -0800

    Update WebMain.js

--- scripts/WebMain.js
+++ scripts/WebMain.js
@@ -499,7 +499,7 @@ var STD = {
 					condmode = false;
 				}
 				else {
-					str += callLib(this, args[i], args[i+1]) + "};";
+					str += callLib(this, args[i], args[i+1]) + "}";
 					condmode = true;
 				}
 			}
